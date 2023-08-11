85b4dbacf7b7572f420b67ed5e9973aa24a9b14b
Igor Santos <bob@example.com>
2023-08-11 19:14:06 -0300

    funcoes atualizadas

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WeatherService } from '../../services/weather.service';
 import { WeatherDatas } from 'src/app/models/interfaces/weatherDatas';
+import { Subject } from 'rxjs';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: [],
 })
-export class WeatherHomeComponent implements OnInit {
+export class WeatherHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
   initialCityName = "São Paulo";
   weatherDatas!: WeatherDatas;
+  searchIcon = faMagnifyingGlass ;
 
   constructor(private weatherService: WeatherService) {}
 
@@ -25,5 +29,14 @@ export class WeatherHomeComponent implements OnInit {
       },
       error: (error) => console.log(error),
     });
+  }
+  onSubmit(): void {
+    this.getWeatherDatas(this.initialCityName);
+    this.initialCityName= '';
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
